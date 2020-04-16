efed3bd7b83e0a98268fa1fcc1b2d798bd77c9d3
Anonymous-Lpz <58832623+Anonymous-Lpz@users.noreply.github.com>
2020-04-16 01:08:43 -0700

    Update verificador.sh

--- verificador.sh
+++ verificador.sh
@@ -2,6 +2,8 @@
 
 
 if [[ Toolbox-v1.sh != Toolbox-v2.sh ]]; then
+clear
+sleep 3
 echo -e "\e[1;31m"
 figlet -f small No Se A Podido Actualizar Toolbox Porque Aun No Hay Una Nueva Actualizacion Disponible
 sleep 1.3
@@ -9,6 +11,7 @@ bash Toolbox-v1.sh
 fi
 if [[ Toolbox-v2.sh != Toolbox-v1.sh ]]; then
 clear
+sleep 3
 echo -e "\e[1;33m"
 figlet -f small Felicidades Has Actualizado Toolbox A La Versión: 1.2
 sleep 1
